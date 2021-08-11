a2135861cbdce088f630dfb3fb4c25de1a33ce3a
mila <liudmyla@teemyco.com>
2021-08-11 23:29:44 +0300

    add initial state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+
+const element = <h1>Hello</h1>
+
+const container = document.getElementById('root');
 
 render(
-  <App />,
-  document.getElementById('root')
+  element,
+  container,
 );
