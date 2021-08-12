068b9ea7174f87cd2a34dbb519477f8c2fde62f0
mila <liudmyla@teemyco.com>
2021-08-13 00:28:57 +0300

    add custom createElement and render methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 
-const element = React.createElement(
-  'h1',
-  { title: 'foo'},
-  'Hello',
-);
+// const element = React.createElement(
+//   'h1',
+//   { title: 'foo'},
+//   'Hello',
+// );
 
 // const element = {
 //   type: 'h1',
@@ -15,16 +15,80 @@ const element = React.createElement(
 //   },
 // };
 
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+          typeof child === 'object'
+            ? child
+            : createTextElement(child)
+      ),
+    }
+  }
+};
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    }
+  }
+};
+
+function render(element, container) {
+  const dom = element.type === 'TEXT_ELEMENT' ?
+    document.createTextNode('')
+    :
+    document.createElement(element.type);
+
+  const isProperty = key => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name =>
+      dom[name] = element.props[name]
+    )
+
+  element.props.children.forEach(child => 
+    render(child, dom)
+  )
+
+  container.appendChild(dom);
+};
+
+const Didact = {
+  createElement,
+  render,
+};
+
+// const element = Didact.createElement(
+//   'div',
+//   { id: 'foo'},
+//   Didact.createElement('a', null, 'bar'),
+//   Didact.createElement('b')
+// );
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id='foo'>
+    <a>bar</a>
+    <b />
+  </div>
+);
+
 const container = document.getElementById('root');
 
-const node = document.createElement(element.type);
-node['title'] = element.props.title;
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-node.appendChild(text);
-container.appendChild(node);
+// const node = document.createElement(element.type);
+// node['title'] = element.props.title;
+// const text = document.createTextNode('');
+// text['nodeValue'] = element.props.children;
+// node.appendChild(text);
+// container.appendChild(node);
 
-// render(
-//   element,
-//   container,
-// );
+Didact.render(
+  element,
+  container,
+);
