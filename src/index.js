133bf4c24cb2a37f9360c30f295ea5d6afe0a325
mila <liudmyla@teemyco.com>
2021-08-12 23:55:31 +0300

    add own render function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const element = <h1>Hello</h1>
+const element = React.createElement(
+  'h1',
+  { title: 'foo'},
+  'Hello',
+);
+
+// const element = {
+//   type: 'h1',
+//   props: {
+//     title: 'foo',
+//     children: 'Hello',
+//   },
+// };
 
 const container = document.getElementById('root');
 
-render(
-  element,
-  container,
-);
+const node = document.createElement(element.type);
+node['title'] = element.props.title;
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children;
+node.appendChild(text);
+container.appendChild(node);
+
+// render(
+//   element,
+//   container,
+// );
