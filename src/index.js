5a70095ed1074359229bb7e3b08be46e32ffba1f
mila <liudmyla@teemyco.com>
2021-08-22 21:13:53 +0300

    add custom render and createElement methods that render using recursion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+// import { render } from 'react-dom';
 
-render(
-  <App />,
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: { nodeValue: text, children: [] },
+  }
+}
+
+function createElement(type, props, ...children ) {
+  return {
+    type,
+    props: { ...props, children: children.map( child => typeof child === 'object' ? child : createTextElement(child)) },
+  };
+};
+
+const isProperty = prop => prop !== 'children';
+
+function render(element, container) {
+  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type);
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(prop => dom[prop] = element.props[prop]);
+
+  const parent = container.appendChild(dom);
+
+  container.appendChild(parent);
+
+  element.props.children.forEach(child =>
+    render(child, dom)
+  );
+
+};
+
+const CustomReact = {
+  render,
+  createElement
+}
+
+/** @jsx CustomReact.createElement */
+const element = (
+  <div>
+    <a href="https://google.com">link</a>
+  </div>
+); 
+
+CustomReact.render(
+  element,
   document.getElementById('root')
 );
